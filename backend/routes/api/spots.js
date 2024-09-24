acf738b986b6ad04efebb33c7a77199c3011ef6b
graysonslater <graysonslater@gmail.com>
2024-09-24 18:43:53 -0400

    initial commit

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -13,12 +13,81 @@ router.get('/:id', async (req, res, next) => {
     
 })
 
+//create spot middleware
+const validateSpot = [
+    check('address')
+      .exists({ checkFalsy: true })
+      .withMessage('needs address'),
+    check('city')
+      .exists({ checkFalsy: true })
+      .withMessage('needs city'),
+    check('state')
+      .exists({ checkFalsy: true })
+      .withMessage('needs state'),
+    check('country')
+      .exists({ checkFalsy: true })
+      .withMessage('needs country'),
+    check('name')
+      .exists({ checkFalsy: true })
+      .withMessage('needs name'),
+    check('description')
+      .exists({ checkFalsy: true })
+      .withMessage('needs description'),
+    check('lat')
+      .exists({ checkFalsy: true })
+      .withMessage('needs lattitude'),
+    check('lng')
+      .exists({ checkFalsy: true })
+      .withMessage('needs longitude'),
+    check('price')
+      .exists({ checkFalsy: true })
+      .withMessage('needs price'),
+    handleValidationErrors
+];
+
+//create a spot
+router.post('/test', validateSpot, async (req, res,) => {
+    console.log(req.body)
+    //get all info from req body
+    const {address, city, state, country, name, description, lat, lng, price} = req.body;
+    //create new instance of spot
+    const newSpot = await Spot.create({
+        address,
+        city,
+        state,
+        country,
+        name,
+        description,
+        lat,
+        lng,
+        price
+    });
+
+    res.status(201)
+    res.json(newSpot)
+
+})
+
+router.get('/test3', async (req,res) => {
+    
+    res.json({message:'hello'})
+})
+
 router.get('/', async (req, res, next) => {
     const allSpots = await Spot.findAll();
     console.log(allSpots);
 
     res.json(allSpots)
 })
-
-
+/* test fetch
+fetch('/api/users', {
+    method: 'POST',
+    headers: {
+      "Content-Type": "application/json",
+      "XSRF-TOKEN": "fWwuoKLy-LxSZ0ezNPW19aNKulQY_LB7ljs0"
+    },
+    body: JSON.stringify({address: "124", city: "test", state: "test", country: "test", name: "test", description: "test", lat: "test", lng: "test", price: "test"
+    })
+  }).then(res => res.json()).then(data => console.log(data));
+*/
 module.exports = router;

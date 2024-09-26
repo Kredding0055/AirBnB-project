93a3f67fed5e97af89d0a0c82c1b45865399e491
graysonslater <graysonslater@gmail.com>
2024-09-26 11:36:35 -0400

    changed error tittle for log in

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -87,7 +87,7 @@ router.post(
       if (!user || !bcrypt.compareSync(password, user.hashedPassword.toString())) {
         const err = new Error('Login failed');
         err.status = 401;
-        // err.title = 'Login failed';
+        err.title = 'Login failed';
         err.message = 'Invalid Credentials';
         return next(err);
       }
